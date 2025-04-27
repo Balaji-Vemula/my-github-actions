c67065e1638a218ab44200a190bf952bc433812a
vemula <vemulabalaji1998@gmail.com>
2025-04-27 17:20:40 +0530

    Added positive case

--- pytest-files/test.py
+++ pytest-files/test.py
@@ -4,7 +4,7 @@ n = 5
 
 def test_comparison():
     print("Running test_comparison")  # Log to see if this test is executed
-    assert n == 6
+    assert n == 5
 
 def test_odd():
     print("Running test_odd")  # Log to see if this test is executed
