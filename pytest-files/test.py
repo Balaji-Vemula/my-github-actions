c7f2f7f8af9e300268e24fb8b06fc57de19ee43e
vemula <vemulabalaji1998@gmail.com>
2025-04-27 16:59:31 +0530

    Updated test.py

--- pytest-files/test.py
+++ pytest-files/test.py
@@ -1,10 +1,14 @@
-# test_sample.py
+# pytest-files/test.py
+
 n = 5
+
 def test_comparison():
+    print("Running test_comparison")  # Log to see if this test is executed
     assert n == 6
 
 def test_odd():
-    if n%2 == 0:
+    print("Running test_odd")  # Log to see if this test is executed
+    if n % 2 == 0:
         assert False, "Got even number"
     else:
         assert True
